e57815b3b090556b568e6d4fb71bc0fb6a5602f7
Paul Harriet Asiñero <harriebird@yahoo.com>
2025-07-28 04:43:41 +0800

    WIP 20250728: Implemented indexDB store for the chart data

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,99 +1,100 @@
 import "@picocss/pico"
 import "./style.css"
 import { createApp, reactive } from "petite-vue"
-import { Chart } from "chart.js"
+import { Chart } from "chart.js/auto"
+import "chartjs-adapter-moment"
+import { Dexie } from "dexie"
 
-const webSocket = new WebSocket('ws://' + location.hostname + '/socket');
+Chart.defaults.font.size = 16
+Chart.defaults.color = "#FFF"
+
+const webSocket = new WebSocket("ws://" + location.hostname + "/socket")
+
+const db = new Dexie("ESP32WebTemp")
+
+db.version(1).stores({
+    readings: "++id, timestamp, temperature"
+})
 
 const store = reactive({
-    currentPage: "home",
-    sensorTemp: 0,
-    chartData: [],
-
-    getCurrentPage() {
-        return this.currentPage;
-    },
-    setCurrentPage(page) {
-        this.currentPage = page;
-    },
+    sensorTemp: -500,
 
     getSensorTemp() {
-        return this.sensorTemp;
+        return this.sensorTemp
     },
     setSensorTemp(temp) {
-        this.sensorTemp = temp;
-    },
-
-    getChartData() {
-        return this.chartData;
-    },
-    addChartData(temp) {
-        let data = {
-            timestamp: new Date().toISOString(),
-            temperature: temp
-        }
-        this.chartData.push(data);
+        this.sensorTemp = temp
     }
-});
+})
 
 function TopNavbar(props) {
     return {
-        $template: "#top-navbar"
-    }
-}
-
-function HomePage(props) {
-    return {
-        $template: "#home-page"
-    }
-}
-
-function ChartPage(props) {
-    return {
-        $template: "#chart-page",
-        mounted() {
-            new Chart(
-                document.getElementById("live-chart"),
-                {
-                    type: "line",
-                    data: {
-                        labels: store.getChartData().map(row => row.timestamp),
-                        datasets: [
-                            {
-                                label: "Temperature in °C (Celsius)",
-                                data: store.getChartData().map(row => row.temperature)
-                            }
-                        ]
-                    }
-                }
-            );
-            console.log("chart created!")
+        $template: "#top-navbar",
+        deleteDB() {
+            db.delete()
+            location.reload()
         }
     }
 }
 
-function AboutPage(props) {
+function TemperatureChart(props) {
     return {
-        $template: "#about-page"
+        $template: "#chart",
+        async renderChart() {
+            const readings = await db.readings.toArray()
+            let liveChart = new Chart(document.getElementById("live-chart"), {
+                type: "line",
+                data: {
+                    labels: readings.map(row => row.timestamp),
+                    datasets: [{
+                        label: "Temperature in °C (Celsius)", data: readings.map(row => row.temperature),
+                    }]
+                },
+                options: {
+                    scales: {
+                        x: {
+                            grid: { color: "#666" },
+                            type: 'time'
+                        },
+                        y: {
+                            grid: { color: "#666" }
+                        }
+                    }
+                }
+            })
+
+            setInterval(async () => {
+                webSocket.send("get_temp");
+                let timestamp = new Date()
+                let reading = store.getSensorTemp()
+                if(reading !== -500) {
+                    liveChart.data.labels.push(timestamp)
+                    liveChart.data.datasets[0].data.push(store.getSensorTemp())
+                    liveChart.update()
+                    await db.readings.add({timestamp: timestamp, temperature: reading})
+                }
+
+            }, 5000)
+        }
+    }
+}
+
+function Temperatures() {
+    return {
+        $template: "#temperatures"
     }
 }
 
 createApp({
     store,
     TopNavbar,
-    HomePage,
-    ChartPage,
-    AboutPage
+    TemperatureChart,
+    Temperatures,
 }).mount("#app");
 
 webSocket.onmessage = (e) => {
     if (!isNaN(e.data)) {
         let temperature = parseFloat(e.data);
         store.setSensorTemp(temperature);
-        store.addChartData(temperature);
     }
 }
-
-let backgroundCollect = setInterval(() => {
-    webSocket.send("get_temp");
-}, 1000);
